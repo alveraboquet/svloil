d773e60e2ac0659b291ad0599388b64690e5b256
Bart de Kinkelaar <bartdekinkelaar@outlook.com>
2022-08-31 20:17:30 +0200

    Current changes commented

--- wp-content/themes/svloil/template-parts/header/sponsors.php
+++ wp-content/themes/svloil/template-parts/header/sponsors.php
@@ -3,9 +3,9 @@
 $mainSponsors = getMainSponsors();
 $countLoop = 0;
 if ($mainSponsors) {
-    echo "<div class='row sponsor-head'>";
+    echo "<div class='row'>";
     foreach ($mainSponsors as $mainSponsor) {
-        echo "<div class='row sponsor-head__item'>";
+        echo "<div class='col'>";
         echo "<img class='sponsorlogo' src='" . get_the_post_thumbnail_url($mainSponsor->ID) . "' />";
         echo '</div>';
         $countLoop++;
@@ -16,5 +16,20 @@ if ($mainSponsors) {
     }
     echo '</div>';
 }
+
+//if ($mainSponsors) {
+//    echo "<div class='row sponsor-head'>";
+//    foreach ($mainSponsors as $mainSponsor) {
+//        echo "<div class='row sponsor-head__item'>";
+//        echo "<img class='sponsorlogo' src='" . get_the_post_thumbnail_url($mainSponsor->ID) . "' />";
+//        echo '</div>';
+//        $countLoop++;
+//
+//        if ($countLoop === 3) {
+//            break;
+//        }
+//    }
+//    echo '</div>';
+//}
 
 wp_reset_postdata();
